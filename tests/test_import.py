94ab1e5e1955d61926fb9da57cdcc29f1042cca9
Tyrone O'Garro Jr <112409778+Scipio94@users.noreply.github.com>
2026-03-13 10:57:33 -0400

    Add files via upload

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,5 +1,9 @@
 import pynjsla
 from pynjsla.njsla import njsla_split
+from pynjsla.njsla_batch import njsla_batch_split
 
 def test_njsla_split_exists():
     assert njsla_split is not None
+
+def test_njsla_batch_split_exists():
+    assert njsla_batch_split is not None
